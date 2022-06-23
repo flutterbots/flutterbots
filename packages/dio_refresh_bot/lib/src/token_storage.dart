1c89c00a478f2933d860c23f3073a382d342f214
hussam <hussamhajjar.it@gmail.com>
2022-06-23 11:38:47 +0300

    refactor: remove BotMemoryTokenStorageWrapper

--- packages/dio_refresh_bot/lib/src/token_storage.dart
+++ packages/dio_refresh_bot/lib/src/token_storage.dart
@@ -27,66 +27,26 @@ mixin BotTokenStorageType<T> on BotStorage<T> {
 
 /// A token storage that extends [BotStorage] to store and retrieve tokens.
 abstract class BotTokenStorage<T extends AuthToken> extends BotStorage<T>
-    with BotStorageMixin<T>, BotTokenStorageType<T>, RefreshBotMixin<T> {}
+    with BotStorageMixin<T>, BotTokenStorageType<T> {}
 
 /// Memory storage to store and retrieve tokens in memory.
 /// read, write, and delete the `value` from memory
-abstract class BotMemoryTokenStorage<T extends AuthToken>
-    extends BotMemoryStorage<T>
-    with BotTokenStorageType<T>, RefreshBotMixin<T> {
+class BotMemoryTokenStorage<T extends AuthToken>
+    extends BotMemoryStorage<T> with BotTokenStorageType<T> {
   @mustCallSuper
   @override
-  Future<void> delete([String? message]) async {
+  FutureOr<void> delete([String? message]) async {
     value = null;
     await super.delete(message);
   }
 
   @override
   @mustCallSuper
-  Future<void> write(T? token) async {
-    value = token;
-    await super.write(token);
+  FutureOr<void> write(T? value) async {
+    this.value = value;
+    await super.write(value);
   }
 
   @override
   T? get initValue => null;
-}
-
-/// Memory storage to store and retrieve tokens in memory.
-/// read, write, and delete the `value` from memory
-class BotMemoryTokenStorageWrapper<T extends AuthToken>
-    extends BotMemoryStorage<T>
-    with BotTokenStorageType<T>, RefreshBotMixin<T> {
-  ///
-  BotMemoryTokenStorageWrapper({
-    WriteCallback<T>? onUpdated,
-    DeleteTokenCallback<T>? onDeleted,
-    T? initValue,
-  })  : _writeCallback = onUpdated,
-        _deleteCallback = onDeleted,
-        _initValue = initValue;
-
-  final WriteCallback<T>? _writeCallback;
-  final DeleteTokenCallback<T>? _deleteCallback;
-  final T? _initValue;
-
-  @mustCallSuper
-  @override
-  FutureOr<void> delete([String? message]) async {
-    await _deleteCallback?.call();
-    value = null;
-    final deleteMessage = (await _deleteCallback?.call()) ?? message;
-    await super.delete(deleteMessage);
-  }
-
-  @override
-  @mustCallSuper
-  FutureOr<void> write(T? token) async {
-    await _writeCallback?.call(token);
-    value = token;
-    await super.write(token);
-  }
-
-  @override
-  T? get initValue => _initValue;
 }
